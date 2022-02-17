16aaed864a8f74f190287d1b8f4c6d2bbf392a35
Joris Monster <jorismonster@Joriss-MacBook-Pro.local>
2022-02-17 16:33:34 +0100

    Hoofdstuk 3 tot 3.24

--- fps/src/main/scala/Joris.scala
+++ fps/src/main/scala/Joris.scala
@@ -3,15 +3,74 @@ package joris
 
 import cats.instances.set
 import scala.{List => sList}
+import cats.instances.list
+import scala.runtime.IntRef
 
-sealed trait List[+A]
+
+sealed trait List[+A]:
+  def foldRight[B](z:B)(f: (A,B) => B): B =
+    this match{
+      case Nil => z
+      case Cons(h,t) => f(h, t.foldRight(z)(f))
+    }
+  def foldLeft[B](z: B)(f: (B,A) => B): B =
+    this match{
+      case Nil => z
+      case Cons(h,t) => t.foldLeft(f(z,h))(f)
+    }
+
+  def sum: Int =  this.asInstanceOf[List[Int]].foldLeft(0)(_+_)
+  def project: Int =  this.asInstanceOf[List[Int]].foldLeft(1)(_*_)
+
+  def length: Int =
+    this match{
+      case Nil => 0
+      case Cons(_,_) => foldLeft(0)((acc,_)=> acc+1)
+    }
+  def append[A1 >: A](a: A1): List[A1] =
+    this.foldRight(Cons(a,Nil))((t,acc) => Cons(t,acc))
+
+  def append[A1 >: A](as:List[A1]): List[A1] =
+    this.foldRight(as)((t,acc)=> Cons(t,acc)) 
+
+  def reverse: List[A] = foldLeft(List.empty)((acc, elm) => (Cons(elm,acc)))
+  
+  def map[B](f:(A => B)): List[B] = 
+    this match{
+      case Nil => Nil
+      case Cons(h,t) => Cons(f(h), t.map(f))
+    }
+  
+  def filter[B](f:(A => Boolean)): List[A] =
+    this match{
+      case Nil => Nil
+      case Cons(h,t) if f(h) => Cons(h, t.filter(f))
+      case Cons(_,t) => t.filter(f)
+    }
+
+  def flatMap[B](f: A=> List[B]):List[B] =
+    concat(map(f))
+  
+  def zipWith[B,C](bs:List[B])(f:(A,B)=> C):List[C] =
+    bs match{
+        case Nil => Nil
+        case Cons(bh,bt) => this match{
+          case Nil => Nil
+          case Cons(h,t) => Cons(f(h,bh),t.zipWith(bt)(f))
+        }
+    }
+  
+
 case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List:
   def apply[A](as: A*):List[A]=
-    as.foldRight[List[A]](Nil)((t,acc)=> Cons(t,acc))
+    as.foldRight(List.empty)((t,acc)=> Cons(t,acc))
+  def empty[A]: List[A] = Nil
 
+def concat[A](a: List[List[A]]):List[A] =
+  a.foldRight(List.empty)((elm, acc) => acc.append(elm))
 
 def tail[A](ls: List[A]): List[A] = 
   ls match {
@@ -57,6 +116,18 @@ def isSorted[A](as:Array[A], ordered:(A,A)=> Boolean): Boolean =
   loop(0,1)
         
 
+def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B) : B =
+        as match
+            case Nil => z
+            case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+
+def length [A] (as: List[A]):Int=
+  foldRight(as,0)((_,y:Int) => y+1)
+
 object Main extends App:
-  val list = List(1,2,3)
-  println(init(list))
+  val list1 = List(4.0,.5,.6)
+  val list2 = List(1,2,3)
+  val list3 = List(7,8,9)
+  val comlist = List(list1,list2,list3)
+  println(list1.flatMap(i =>if(i>2) List(i) else Nil))
+  println(list2.zipWith(list3)(_+_))
